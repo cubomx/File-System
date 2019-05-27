3d1518b670ce08c64fe846176525ca105c571efe
Cubo <cheservelram657@gmail.com>
2019-05-26 19:00:41 -0500

    Additions
    Already can get files from computer to the virtual memory. Also showing the files in the virtual memory and the blocks in which this files are.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@
 #include <sstream>
 #include <algorithm>
 #include <math.h>
+#include <map>
+#include <set>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
 
 using namespace std;
 
@@ -12,8 +17,10 @@ struct fileSystem {
     string name;
     int sizeBlock;
     int availableBlocks;
+    bool notSpace = false;
     int totalBlocks;
-    int startData;
+    set<int> usedBlocks;
+    map <string, vector<int>> files;
     char * memory;
 };
 
@@ -27,12 +34,21 @@ fileSystem create (vector <string>, fileSystem);
 
 void save (fileSystem);
 
+void info (fileSystem);
+
+fileSystem load (fileSystem, vector<string>);
+
+fileSystem allocate(fileSystem, string, string);
+
+void listFiles(fileSystem);
+
+void details(fileSystem, vector<string> cmd);
+
 int main() {
     vector <string> cmd;
     fileSystem file;
     do{
         cmd = gettingCommand();
-
         if ("create" == cmd[0] && cmd.size() == 4){
             file = create(cmd, file);
         }
@@ -42,11 +58,11 @@ int main() {
         else if ("download" == cmd[0]){
 
         }
-        else if ("load" == cmd[0]){
-
+        else if ("load" == cmd[0] && cmd.size() == 3){
+            file = load(file, cmd);
         }
         else if ("ls" == cmd[0]){
-
+            listFiles(file);
         }
         else if ("open" == cmd[0]){
 
@@ -55,11 +71,11 @@ int main() {
 
         }
         else if ("info" == cmd[0]){
-
+            info(file);
         }
 
         else if ("details" == cmd[0]){
-
+            details(file, cmd);
         }
     }while(cmd[0] != "exit");
     return 0;
@@ -72,6 +88,10 @@ void initialCommit(){
 bool isNumeric(string& input) {
     return std::all_of(input.begin(), input.end(), ::isdigit);
 }
+
+/*
+ * Receiving the command by the user
+ */
 
 vector <string> gettingCommand(){
     string  cmd;
@@ -92,6 +112,10 @@ vector<string> split(string str, char delimiter) {
     return internal;
 }
 
+/*
+ * Allocation of the neccessary virtual memory
+ */
+
 fileSystem create (vector <string> cmd, fileSystem file){
     if (isNumeric(cmd[2]) && isNumeric(cmd[3])){
         int blockSize = stoi(cmd[2]);
@@ -101,7 +125,6 @@ fileSystem create (vector <string> cmd, fileSystem file){
                 file.availableBlocks = blockNum;
                 file.totalBlocks = blockNum;
                 file.sizeBlock = blockSize;
-                file.startData = blockNum/10; // The System requires 10 percent for storing where the files are
                 file.name = cmd[1];
                 file.memory = (char*) malloc(blockNum*blockSize);
                 for(int n =0; n < blockNum*blockSize;n++)
@@ -126,8 +149,117 @@ fileSystem create (vector <string> cmd, fileSystem file){
 void save (fileSystem file){
     ofstream mySystem;
     mySystem.open(file.name + ".txt");
-    mySystem << file.name << " " << file.sizeBlock << " " << file.totalBlocks << " " << file.availableBlocks << " " << file.startData << "\n";
+    mySystem << file.name << " " << file.sizeBlock << " " << file.totalBlocks << " " << file.availableBlocks  << "\n";
     mySystem << file.memory;
     mySystem.close();
 }
 
+/*
+ * Showing the information about the file system and the use of its space in memory
+ */
+void info (fileSystem file){
+    if (file.name != ""){
+        int blocksInUse = file.totalBlocks - file.availableBlocks;
+        cout << "FYLE SYSTEM: " << file.name << "\n" << "TOTAL SPACE: " << file.sizeBlock*file.totalBlocks << " BYTES\n";
+        cout << "SPACE IN USE " << file.sizeBlock*blocksInUse << " BYTES\n" << "SPACE FREE: " << file.sizeBlock*file.availableBlocks << "\n";
+    }
+    else{
+        cout << "NOT CURRENT FILE SYSTEM AVAILABLE\n";
+    }
+}
+
+/*
+ * Getting files from computer and processing into the virtual memory
+ */
+
+fileSystem load (fileSystem file, vector <string> cmd){
+
+    string data;
+    ifstream inFile;
+    inFile.open(cmd[1]);
+    if (inFile.is_open()){
+        string buffer;
+        while(getline(inFile, buffer)){
+            data += buffer;
+        }
+        file = allocate(file, data, cmd[2]);
+        if(file.notSpace){
+            cout << "NOT ENOUGH SPACE FOR FILE\n";
+        }
+        else{
+            cout << "SUCCESFULLY LOADED INTO MEMORY\n";
+        }
+    }
+
+    else{
+        cout << "ERROR: FILE NOT FOUND\n";
+    }
+    inFile.close();
+
+
+    return file;
+}
+
+/*
+ * Searching for space in the memory and then saving data into the virtual memory.
+ */
+
+fileSystem allocate(fileSystem file, string data, string name){
+    int block = 0, index = 0;
+    int size = file.sizeBlock;
+    srand(time(NULL));
+    while (index < data.length()){
+        block = rand() % file.totalBlocks;
+        if(file.availableBlocks == 0){
+            file.notSpace = true;
+            break;
+        }
+        else if (file.usedBlocks.count(block) == 0){
+            file.usedBlocks.insert(block);
+            file.files[name].push_back(block);
+            file.availableBlocks--;
+            int pos = block*size;
+            while (pos < (block+1)*size && index < data.length()){
+                file.memory[pos] = data[index];
+                index++;
+                pos++;
+            }
+        }
+    }
+    return file;
+}
+
+/*
+ * Showing the files that are in the virtual memory
+ */
+
+void listFiles(fileSystem file){
+    cout << "FILES: \n";
+    for (auto const& x : file.files)
+    {
+        cout << "  " <<  x.first << "\n";
+    }
+    if (file.files.size() == 0){
+        cout << "NOT FILES IN MEMORY\n";
+    }
+}
+
+/*
+ * Searching if file is in the virtual memory.
+ * If it is, then show where blocks are in.
+ */
+
+void details(fileSystem file, vector <string> cmd){
+    if (file.files.find(cmd[1]) == file.files.end()){
+        cout << "NOT SUCH FILE AVAILABLE\n";
+    }
+    else{
+        vector<int> blocks = file.files[cmd[1]];
+        cout << "# BLOCKS: " << blocks.size() << "\n";
+        cout << "BLOCKS IN: ";
+        for (auto block =  blocks.begin(); block != blocks.end(); block++){
+            cout << *block << " ";
+        }
+        cout << "\n";
+    }
+}
